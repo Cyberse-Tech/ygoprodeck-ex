b5600ac5f44e194239da0cd1094a426499552942
Mau Abata <mauabata@gmail.com>
2026-01-12 14:30:50 -0600

    Fix error if Req not available and http is run through Finch

--- lib/ygoprodeck/http/req.ex
+++ lib/ygoprodeck/http/req.ex
@@ -6,28 +6,38 @@ defmodule YGOProDeck.HTTP.Req do
 
       config :ygoprodeck,
         http_client: YGOProDeck.HTTP.Req
+
+  Requires the `req` package to be installed.
   """
 
   @behaviour YGOProDeck.HTTP
 
   @impl true
   def get(url, opts \\ []) do
+    unless Code.ensure_loaded?(Req) do
+      raise ArgumentError, """
+      Req is not installed. Please add to your deps:
+
+          {:req, "~> 0.5"}
+      """
+    end
+
     timeout = opts[:timeout] || 10_000
 
     case Req.get(url, receive_timeout: timeout) do
-      {:ok, %Req.Response{status: 200, body: body}} ->
+      {:ok, %{status: 200, body: body}} ->
         {:ok, body}
 
-      {:ok, %Req.Response{status: 404}} ->
+      {:ok, %{status: 404}} ->
         {:error, :not_found}
 
-      {:ok, %Req.Response{status: 429}} ->
+      {:ok, %{status: 429}} ->
         {:error, :rate_limited}
 
-      {:ok, %Req.Response{status: status, body: body}} when status >= 500 ->
+      {:ok, %{status: status, body: body}} when status >= 500 ->
         {:error, {:api_error, "Server error #{status}: #{body}"}}
 
-      {:ok, %Req.Response{status: status, body: body}} ->
+      {:ok, %{status: status, body: body}} ->
         {:error, {:api_error, "HTTP #{status}: #{body}"}}
 
       {:error, %Mint.TransportError{reason: reason}} ->
